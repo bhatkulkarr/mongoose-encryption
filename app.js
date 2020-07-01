985e04fa19f732d9abaa4eccfc203131b47e1765
Rajat_Bhatkulkar <rajatbhatkulkar@gmail.com>
2020-07-01 18:55:20 +0530

    Added Hash encryption

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", 'ejs');
@@ -21,9 +20,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 
 
@@ -47,7 +43,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function(err){
@@ -62,7 +58,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, userfound){
 
